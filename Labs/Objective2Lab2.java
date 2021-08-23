ea1dc4301ca2202de1f99347f9cfa2cf58b677fb
Gramman87 <gramman87@gmail.com>
2021-08-23 13:02:22 -0600

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Graham Anderson";
+    int age = 33;
+    String state = "Colorado";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
